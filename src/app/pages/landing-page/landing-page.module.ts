f3a51859e7763e54e8ddf519f6a97721074a50fc
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-06-24 21:17:15 -0300

    chore: Inicializando componentes landing-page

--- src/app/pages/landing-page/landing-page.module.ts
+++ src/app/pages/landing-page/landing-page.module.ts
@@ -1,6 +1,8 @@
 import { RouterModule, Routes } from "@angular/router";
 import { LandingPageComponent } from "./landing-page.component";
 import { NgModule } from "@angular/core";
+import { HeaderComponent } from './components/header/header.component';
+import { MainButtonComponent } from './components/main-button/main-button.component';
 
 const routes: Routes = [
     { path: '', component: LandingPageComponent }
@@ -9,6 +11,6 @@ const routes: Routes = [
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule],
-    declarations: [LandingPageComponent]
+    declarations: [LandingPageComponent, HeaderComponent, MainButtonComponent]
 })
 export class LandingPageModule { }
