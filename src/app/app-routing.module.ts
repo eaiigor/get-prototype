860c4b14ccc6a19ec86061cb5a0dce0b4c751f13
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-06-24 21:04:08 -0300

    chore: Iniciando roteamento de páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/landing-page/landing-page.module').then(
+        (m) => m.LandingPageModule,
+      ),
+  },
+  {
+    path: 'to-do-list',
+    loadChildren: () =>
+      import('./pages/to-do-list/to-do-list.module').then(
+        (m) => m.ToDoListModule,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
